41f98695fda6b9de480880c64b2595f8ca9ac49d
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-05-23 16:59:31 +0200

    css connexion page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import AllRoutes from "@components/AllRoutes";
+import NavigationBar from "@components/NavBar";
 
 function App() {
   return (
     <Router>
+      <NavigationBar />
       <AllRoutes />
     </Router>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+function NavigationBar() {
+  return (
+    <nav>
+      <a href="/">Accueil</a>
+      <a href="/dashboard">Gestion</a>
+      <a href="/connexion">Connexion</a>
+    </nav>
+  );
+}
+export default NavigationBar;
